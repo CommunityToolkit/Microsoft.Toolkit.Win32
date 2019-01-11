d754a203e6afe248a3db924d9149f1271cc5f9ba
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2019-01-10 17:58:25 -0800

    Added script to download and install correct dotnet core sdk.

---
+++ build/Install-DotNetCoreSDK.ps1
@@ -0,0 +1,5 @@
+﻿$globalJson = Get-Content "..\global.json" | ConvertFrom-Json
+
+Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1';
+
+./dotnet-install.ps1 -Version $globalJson.sdk.version;
