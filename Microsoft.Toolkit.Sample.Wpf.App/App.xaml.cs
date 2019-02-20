3401e8d1868a6aa37a77f65b95225f537f64ea89
devsko <devsko@outlook.com>
2019-02-20 22:49:17 +0100

    Example of WindowsXamlHostBase.ApplicationCreated

--- Microsoft.Toolkit.Sample.Wpf.App/App.xaml.cs
+++ Microsoft.Toolkit.Sample.Wpf.App/App.xaml.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Toolkit.Wpf.UI.XamlHost;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -19,5 +20,24 @@ namespace Microsoft.Toolkit.Sample.Wpf.App
     public partial class App : Application
 #pragma warning restore CA1724 // Type names should not match namespaces
     {
+        public App()
+        {
+            WindowsXamlHostBase.ApplicationCreated += (sender, args) =>
+            {
+                if (sender is global::Windows.UI.Xaml.Application xamlApplication)
+                {
+                    // Setting dark theme for the XAML island even when windows is set to light theme.
+                    xamlApplication.RequestedTheme = global::Windows.UI.Xaml.ApplicationTheme.Dark;
+
+                    // You can include .xbf files (XAML Binary Format) from an UWP app as described in 
+                    // https://blogs.windows.com/buildingapps/2018/11/08/xaml-islands-a-deep-dive-part-2/
+                    // The .xbf can define any XAML like pages and styles. Here you can create the 
+                    // ResourceDictionary that will be used by the XAML in the island.
+
+                    // var stylesUri = new Uri("ms-appx:///App/Styles/AppStyles.xaml");
+                    // xamlApplication.Resources = new global::Windows.UI.Xaml.ResourceDictionary { Source = stylesUri };
+                }
+            };
+        }
     }
 }
