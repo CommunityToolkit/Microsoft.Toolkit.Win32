17bbe7ba13a2fcc4408bced0de6938ef261395c4
Oscar Calvo <ocalvo@microsoft.com>
2019-05-16 14:19:23 -0700

    Fix build break

--- Microsoft.Toolkit.Win32.UI.XamlApplication/pch.h
+++ Microsoft.Toolkit.Win32.UI.XamlApplication/pch.h
@@ -3,8 +3,6 @@
 #include <SDKDDKVer.h>
 //#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
-//#include <roapi.h>
-//#include <libloaderapi.h>
 #include <WinUser.h>
 
 #include <unknwn.h> // To enable support for non-WinRT interfaces, unknwn.h must be included before any C++/WinRT headers.
@@ -12,5 +10,3 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.UI.Xaml.Hosting.h>
 #include <winrt/Windows.System.h>
-
-#include <winres.h>
