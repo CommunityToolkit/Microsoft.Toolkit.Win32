24ee4256279f35ee924f4bb69e31097199ac0b81
Oscar Calvo <ocalvo@microsoft.com>
2019-07-19 11:51:37 -0700

    Adds InitializeComponent() Apply PR feedback

--- Microsoft.Toolkit.Sample.DesktopWindow/MainWindow.xaml.cs
+++ Microsoft.Toolkit.Sample.DesktopWindow/MainWindow.xaml.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using Windows.UI.Xaml;
+using Microsoft.Toolkit.Win32.UI.XamlHost;
 
 namespace Microsoft.Toolkit.Sample.DesktopWindow
 {
-    class MainWindow: Microsoft.Toolkit.Win32.UI.XamlHost.DesktopWindow
+    internal partial class MainWindow: Microsoft.Toolkit.Win32.UI.XamlHost.DesktopWindow
     {
         public MainWindow()
         {
-            global::System.Uri resourceLocator = new global::System.Uri("ms-appx:///MainWindow.xaml");
-            global::Windows.UI.Xaml.Application.LoadComponent(this, resourceLocator, global::Windows.UI.Xaml.Controls.Primitives.ComponentResourceLocation.Application);
+            this.InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Hello from Xaml!");
         }
     }
+
+    partial class MainWindow : global::Windows.UI.Xaml.Markup.IComponentConnector,
+        global::Windows.UI.Xaml.Markup.IComponentConnector2
+    {
+        public void Connect(int connectionId, object target)
+        {
+            switch (connectionId)
+            {
+                case 2: // MainPage.xaml line 12
+                    {
+                        var element = (global::Windows.UI.Xaml.Controls.Button)(target);
+                        element.Click += this.OnButtonClick;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// GetBindingConnector(int connectionId, object target)
+        /// </summary>
+        public global::Windows.UI.Xaml.Markup.IComponentConnector GetBindingConnector(int connectionId, object target)
+        {
+            return null;
+        }
+    }
+
 }
---
+++ Microsoft.Toolkit.Win32.UI.XamlHost/DesktopApp/DesktopWindowExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Microsoft.Toolkit.Win32.UI.XamlHost
+{
+    public static class DesktopWindowExtensions
+    {
+        public static void InitializeComponent(this DesktopWindow @this)
+        {
+            var resourceLocator = new global::System.Uri($"ms-appx:///{@this.GetType().Name}.xaml");
+            global::Windows.UI.Xaml.Application.LoadComponent(@this, resourceLocator, global::Windows.UI.Xaml.Controls.Primitives.ComponentResourceLocation.Application);
+        }
+    }
+}
